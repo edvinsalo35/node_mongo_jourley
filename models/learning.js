af6e4c237dd7d9ea67499f725bfbfbeed1d0e151
Edvin Salo <edvin.salo35@hotmail.com>
2016-06-17 11:52:56 +0200

    schedule column attached

--- models/learning.js
+++ models/learning.js
@@ -5,6 +5,7 @@ var learningSchema = new Schema({
 	title: String,
 	reads: Number,
 	image: String,
+	schedule: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('Learning', learningSchema);
